17ea39bfa89743cd84111bf9c6cb8a3da16dbdef
Josue Jacob <josuejacob.oru@gmail.com>
2024-03-28 19:47:06 -0600

    Commmit con el header y el footer

--- introduccion.py
+++ introduccion.py
@@ -1,3 +1,9 @@
+#INTRODUCTION OF THE PYTHON
+# HEADER
+#HEADER 2
+#HEADER 3
+
+
 a = "Josue Jacob"
 b = 24
 c = "male"
@@ -7,3 +13,7 @@ print(a);
 print(b);
 print(c);
 print(d);
+
+# FOOTHER
+#FOOTER 2
+# FOOTER 3
